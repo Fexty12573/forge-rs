81a58529bb16f98d82a5f3e7f5e5ec15729bec39
Fexty <bogofogo1@gmail.com>
2026-04-22 10:57:18 +0200

    Improve target spec

--- cargo-forge/src/embedded.rs
+++ cargo-forge/src/embedded.rs
@@ -2,5 +2,5 @@ pub const SWITCH32_LD: &str = include_str!("../templates/switch32.ld");
 pub const EXPORTED_TXT: &str = include_str!("../templates/exported.txt");
 pub const RUST_TOOLCHAIN: &str = include_str!("../templates/rust-toolchain.toml");
 
-pub const TARGET_JSON: &str = include_str!("../../targets/armv8a-none-eabi.json");
-pub const TARGET_NAME: &str = "armv8a-none-eabi";
+pub const TARGET_JSON: &str = include_str!("../../targets/armv7a-none-eabi.json");
+pub const TARGET_NAME: &str = "armv7a-none-eabi";
