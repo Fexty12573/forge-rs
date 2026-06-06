3b541b0d5db1867942ee8a0314c741bd92f48015
Fexty12573 <bogofogo1@gmail.com>
2026-06-06 22:01:28 +0200

    Remove unused socket constants

--- forge/src/socket/mod.rs
+++ forge/src/socket/mod.rs
@@ -5,15 +5,9 @@ pub use tcp_server::TcpServer;
 pub use tcp_stream::{Shutdown, TcpStream};
 
 mod consts {
-    pub const AF_LOCAL: i32 = 1;
-    pub const AF_UNIX: i32 = 1;
     pub const AF_INET: i32 = 2;
-    pub const AF_INET6: i32 = 28;
 
     pub const SOCK_STREAM: i32 = 1;
-    pub const SOCK_DGRAM: i32 = 2;
-    pub const SOCK_RAW: i32 = 3;
-    pub const SOCK_SEQPACKET: i32 = 5;
 
     pub const MSG_PEEK: i32 = 0x2;
 
