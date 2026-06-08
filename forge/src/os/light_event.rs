701e147931abbf7ef22957df7dc0b3bd400c7385
Fexty <bogofogo1@gmail.com>
2026-06-08 17:38:57 +0200

    Add LightEvent API

---
+++ forge/src/os/light_event.rs
@@ -0,0 +1,50 @@
+use core::time::Duration;
+
+use sys::os::{EventClearMode, TimeSpanType, light_event::*};
+
+pub struct LightEvent {
+    inner: LightEventType,
+}
+
+impl LightEvent {
+    pub fn new(signaled: bool, clear_mode: EventClearMode) -> Self {
+        let mut inner = LightEventType::default();
+        unsafe { forge_nnosInitializeLightEvent(&mut inner, signaled, clear_mode) };
+        Self { inner }
+    }
+
+    pub fn finalize(&mut self) {
+        unsafe { forge_nnosFinalizeLightEvent(self.ptr()) };
+    }
+
+    pub fn signal(&mut self) {
+        unsafe { forge_nnosSignalLightEvent(self.ptr()) };
+    }
+
+    pub fn wait(&mut self) {
+        unsafe { forge_nnosWaitLightEvent(self.ptr()) };
+    }
+
+    pub fn try_wait(&mut self) -> bool {
+        unsafe { forge_nnosTryWaitLightEvent(self.ptr()) }
+    }
+
+    pub fn wait_timeout(&mut self, timeout: Duration) -> bool {
+        let timeout = timeout.as_nanos() as u64;
+        unsafe { forge_nnosTimedWaitLightEvent(self.ptr(), TimeSpanType(timeout)) }
+    }
+
+    pub fn clear(&mut self) {
+        unsafe { forge_nnosClearLightEvent(self.ptr()) };
+    }
+
+    pub(crate) fn ptr(&mut self) -> *mut LightEventType {
+        &mut self.inner
+    }
+}
+
+impl Drop for LightEvent {
+    fn drop(&mut self) {
+        self.finalize();
+    }
+}
--- forge/src/os/mod.rs
+++ forge/src/os/mod.rs
@@ -1,5 +1,6 @@
 pub mod barrier;
 pub mod condvar;
 pub mod event;
+pub mod light_event;
 pub mod lock;
 pub mod mutex;
